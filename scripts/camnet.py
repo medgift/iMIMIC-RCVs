e0e59ff00fc561abaf77978ee2d11e90ae09cb11
maragraziani <1537639@gmail.com>
2018-08-27 17:45:51 -0400

    add camnet config parser

--- scripts/camnet.py
+++ scripts/camnet.py
@@ -1,16 +1,37 @@
-import functions
+try:
+    import configparser as ConfigParser
+except ImportError:
+    import ConfigParser
+
 import keras
 from keras import backend as K
+
+def parseTrainingOptions(configFile):
+    settings = {}
+    config = ConfigParser.RawConfigParser(allow_no_value = True)
+    config.read(configFile)
+    settings['model_type'] = config.get("train", "model_type")
+    settings['activation'] = config.get("train", "activation")
+    settings['loss'] = config.get("train", "loss")
+    settings['lr'] = float(config.get("train", "lr"))
+    settings['decay'] = float(config.get("train", "decay"))
+    settings['momentum'] = float(config.get("train", "momentum"))
+    settings['nesterov'] = config.get("train", "nesterov")
+
+    settings['batch_size'] = int(config.get("train", "batch_size"))
+    settings['epochs'] = int(config.get("train", "epochs"))
+    settings['verbose'] = int(config.get("train", "verbose"))
+    return settings
 
 class Camnet():
     config_file_path=''
 
     def __init__(self, config_file_path='./config.cfg'):
-        self.config_file_path=config_file_path 
-    
+        self.config_file_path=config_file_path
+
     def get_Model(self, config_file_path):
         settings = functions.parseTrainingOptions(config_file_path)
-        
+
         if settings['model_type']=='resnet':
             base_model = keras.applications.resnet50.ResNet50(
                                                               include_top=True,
@@ -36,4 +57,3 @@ class Camnet():
                       metrics=['accuracy']
                       )
         return model
-        
